08abfa43902ffc41fefbc67b83b6f3b1c81a3559
Uli Schrey <uli.schrey@web.de>
2020-11-13 20:32:16 +0100

    refactored version with MQTT publish with config in yaml

--- go.mod
+++ go.mod
@@ -5,7 +5,11 @@ go 1.15
 require (
 	github.com/antchfx/xmlquery v1.3.3
 	github.com/antchfx/xpath v1.1.10 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/go-co-op/gocron v0.3.3
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/jasonlvhit/gocron v0.0.1
 	github.com/mclane/xmlquery v1.3.4-0.20201111142412-bd3f7225372f
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
+	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -10,8 +10,50 @@ import (
 	"strings"
 	"time"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/antchfx/xmlquery"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/go-co-op/gocron"
 )
+
+type Config struct {
+	Dwdstation string `yaml:"dwdstation"`
+
+	Pvsetup struct {
+		Peakpower        float64 `yaml:"peakpower"`
+		Efficiencyfactor float64 `yaml:"efficiencyfactor"`
+	} `yaml:"pvsetup"`
+
+	Evcc struct {
+		Pvthreshold float64 `yaml:"pvthreshold"`
+		Basicload   float64 `yaml:"basicload"`
+	} `yaml:"evcc"`
+
+	Mqttbroker struct {
+		Name        string `yaml:"name"`
+		Port        string `yaml:"port"`
+		User        string `yaml:"user"`
+		Password    string `yaml:"password"`
+		PublishTime string `yaml:"publishTime"`
+	} `yaml:"mqttbroker"`
+}
+
+var Cfg Config
+
+func (c *Config) getConf() *Config {
+
+	yamlFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		panic(err)
+	}
+
+	return c
+}
 
 func readZipFile(zf *zip.File) ([]byte, error) {
 	f, err := zf.Open()
@@ -22,16 +64,16 @@ func readZipFile(zf *zip.File) ([]byte, error) {
 	return ioutil.ReadAll(f)
 }
 
-func main() {
-
-	// DWD cde for the nearest DWD station
-	const DWDstation = "P444"
+func readDWDData(st string, pvarea float64) ([]int64, []float64) {
+	// slices containing DWD data (timestamp and 1h pv energy data in Wh)
+	// conversion of kJ to Wh
+	const WhperkJ = 0.2777777
 
 	var zeit = []int64{}
 	var radWert = []float64{}
 
 	// Get data from DWD
-	resp, err := http.Get("http://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/" + DWDstation + "/kml/MOSMIX_L_LATEST_" + DWDstation + ".kmz")
+	resp, err := http.Get("http://opendata.dwd.de/weather/local_forecasts/mos/MOSMIX_L/single_stations/" + st + "/kml/MOSMIX_L_LATEST_" + st + ".kmz")
 	if err != nil {
 		panic(err)
 	}
@@ -80,7 +122,8 @@ func main() {
 				if err1 != nil {
 					panic(err1)
 				}
-				radWert = append(radWert, vv)
+				// directly store PV power data
+				radWert = append(radWert, vv*WhperkJ*pvarea)
 			}
 		}
 	}
@@ -92,6 +135,125 @@ func main() {
 
 	// printout
 	for i, z := range zeit {
-		fmt.Printf("Index: %d Zeit: %d Wert: %.f\n", i, z, radWert[i])
+		fmt.Printf("Index: %d Zeit: %s Wert: %.f\n", i, time.Unix(z, 0), radWert[i])
 	}
+	return zeit, radWert
+}
+
+func calcPVChargeTime(zeit []int64, radWert []float64, pvthr, bload float64) (int, int) {
+	// calculate sum, time above thresholds and max values
+	today := time.Now()
+	tomorrow := time.Date(today.Year(), today.Month(), today.Day(), 0, 0, 0, 0, time.Local)
+	tomorrow = tomorrow.AddDate(0, 0, 1)
+	t1 := tomorrow.Unix()
+	t2 := tomorrow.AddDate(0, 0, 1).Unix()
+	sumkWh := 0.0
+	maxkW := 0.0
+	timeabovepv := 0
+	timeabovebload := 0
+	for i, z := range zeit {
+		if z >= t1 && z <= t2 {
+			sumkWh = sumkWh + radWert[i]
+			if radWert[i] > maxkW {
+				maxkW = radWert[i]
+			}
+			if radWert[i] > pvthr {
+				timeabovepv++
+			}
+			if radWert[i] > bload {
+				timeabovebload++
+			}
+		}
+	}
+
+	// printout resuts
+	fmt.Printf("erwartete Energiegewinnung morgen: %.2f kWh\n", sumkWh/1000)
+	fmt.Printf("Maximalleistung morgen: %.2f kW\n", maxkW/1000)
+	fmt.Printf("Zeit oberhalb PV Schwelle: %d h\n", timeabovepv)
+	fmt.Printf("Zeit oberhalb Basis Schwelle: %d h\n", timeabovebload)
+
+	return timeabovepv, timeabovebload
+}
+
+// MQTT callback functions
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	// do nothing
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("MQTT verbunden")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("MQTT Verbindung verloren: %v", err)
+}
+
+//
+func publishToMQTT(message, bname, bport, buser, bpwd string) {
+	// connect to MQTT broker
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%s", bname, bport))
+	opts.SetClientID("evcc-predict")
+	opts.SetUsername(buser)
+	opts.SetPassword(bpwd)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	token := client.Publish("evcc/loadpoints/0/mode/set", 0, false, message)
+	token.Wait()
+	fmt.Printf("Mode %s published\n", message)
+}
+
+func check() {
+	// copy stuff from cfg
+	DWDstation := Cfg.Dwdstation
+	pvarea := Cfg.Pvsetup.Peakpower * Cfg.Pvsetup.Efficiencyfactor
+	thr := Cfg.Evcc.Pvthreshold
+	bload := Cfg.Evcc.Basicload
+	broker := Cfg.Mqttbroker.Name
+	port := Cfg.Mqttbroker.Port
+
+	// get DWD data
+	zeit, radWert := readDWDData(DWDstation, pvarea)
+
+	// calculate threshold times
+	timeabovepv, timeabovebload := calcPVChargeTime(zeit, radWert, thr, bload)
+
+	// define evcc charging mode
+	mode := ""
+	if timeabovepv >= 2 {
+		mode = "pv"
+	} else {
+		if timeabovebload >= 0 {
+			mode = "minpv"
+		} else {
+			mode = "now"
+		}
+
+	}
+	fmt.Printf("EVCC Mode: %s\n", mode)
+
+	// publish mode for next day
+	publishToMQTT(mode, broker, port, "", "")
+}
+
+func main() {
+
+	// read yaml
+	Cfg.getConf()
+
+	// start cron job
+	publishTime := Cfg.Mqttbroker.PublishTime
+	s := gocron.NewScheduler(time.UTC)
+	_, err := s.Every(1).Day().At(publishTime).Do(check)
+	if err != nil {
+		panic(err)
+	}
+
+	// leave it running
+	s.StartBlocking()
 }
